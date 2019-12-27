dfa8bb8efa4b4920408ab26e00df69bbd6560732
Rameel Azhungal Chellattan Rehim <rameelster@gmail.com>
2019-12-28 02:48:28 +0400

    Updates

---
+++ precache-manifest.004502c68a5fdb99cd7aac7613f19e3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45cea62090d6c6c4077eccfe5e8f9ad5",
+    "url": "/robopwa/index.html"
+  },
+  {
+    "revision": "ed45b9e12305d2cbdcf1",
+    "url": "/robopwa/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e029eb5f8c2f1fbf39d1",
+    "url": "/robopwa/static/css/main.44adcaa1.chunk.css"
+  },
+  {
+    "revision": "ed45b9e12305d2cbdcf1",
+    "url": "/robopwa/static/js/2.2b47b015.chunk.js"
+  },
+  {
+    "revision": "e029eb5f8c2f1fbf39d1",
+    "url": "/robopwa/static/js/main.dfcc1f1d.chunk.js"
+  },
+  {
+    "revision": "466e3cb17cd3cdfbca8b",
+    "url": "/robopwa/static/js/runtime~main.33760ce2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robopwa/static/media/SEGA.12a3db8b.woff"
+  }
+]);
